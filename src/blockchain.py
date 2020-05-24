1aaee94edca35bdabd5a57ce831750e9901dfd00
justinepdevasia <justinepdevasia@gmail.com>
2020-05-24 19:38:50 +0530

    mini-blockchain simple python implementation

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,30 +1,30 @@
 from block import Block
 
 class Blockchain:
-    def __init__(self):
- 	    self.chain = []
-    	self.all_transactions = []
-      	self.genesis_block()
-    
-    def genesis_block(self):
+	def __init__(self):
+		self.chain = []
+		self.all_transactions = []
+		self.genesis_block()
+	
+	def genesis_block(self):
 		transactions = {}
 		gen_block = Block(transactions,"0")
 		self.chain.append(gen_block)
 		return self.chain
 
-    def add_block(self, transactions):
-	    previous_block_hash = self.chain[len(self.chain)-1].hash
-	    new_block = Block(transactions,previous_block_hash)
-	    self.chain.append(new_block)
+	def add_block(self, transactions):
+		previous_block_hash = self.chain[len(self.chain)-1].hash
+		new_block = Block(transactions,previous_block_hash)
+		self.chain.append(new_block)
 
 	def validate_chain(self):
-	    for i in range(1, len(self.chain)):
-	    	current = self.chain[i]
-	    	previous = self.chain[i-1]
-	    	if(current.hash != current.generate_hash()):
-	    		print("The current hash of the block does not equal the generated hash of the block.")
-	    		return False
-	    	if(current.previous_hash != previous.generate_hash()):
-	    		print("The previous block's hash does not equal the previous hash value stored in the current block.")
-	    		return False
-	    	return True
+		for i in range(1, len(self.chain)):
+			current = self.chain[i]
+			previous = self.chain[i-1]
+			if(current.hash != current.generate_hash()):
+				print("The current hash of the block does not equal the generated hash of the block.")
+				return False
+			if(current.previous_hash != previous.generate_hash()):
+				print("The previous block's hash does not equal the previous hash value stored in the current block.")
+				return False
+			return True
